5f0f1f5ec40c3e7c98d5baa160c059465cfb9c29
cxcarvaj <cxcarvaj@gmail.com>
2025-03-17 16:35:23 -0500

    Initial Commit

---
+++ FirstAppSwiftUI/FirstAppSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppSwiftUIApp.swift
+//  FirstAppSwiftUI
+//
+//  Created by Carlos Xavier Carvajal Villegas on 17/3/25.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
